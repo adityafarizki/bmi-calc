f18029ebf5adff442344480cb812c27fffdd36ae
adityafarizki <aditya.farizki1@gmail.com>
2021-07-25 19:26:35 +0700

    add basic program

--- app/bmi_calculator.py
+++ app/bmi_calculator.py
@@ -1,1 +1,46 @@
+import math
 
+
+class BMICalculator:
+    # range is in [a, b)
+    underweight_range = [0, 18.5]
+    normal_range = [18.5, 25]
+    overweight_range = [25, 2 ** 32 - 1]
+
+    def __init__(self, weight_in_kg: float, height_in_m: float):
+        if weight_in_kg <= 0:
+            raise ValueError("Invalid weight value")
+
+        if height_in_m <= 0:
+            raise ValueError("Invalid height value")
+
+        self.weight = weight_in_kg
+        self.height = height_in_m
+
+    def get_bmi_value(self) -> float:
+        if self.height <= 0:
+            raise ValueError("Invalid height value")
+
+        bmi_value = self.weight / (self.height ** 2)
+        # round to 2 decimal places
+        bmi_value = int(bmi_value * 100) / 100
+
+        return bmi_value
+
+    def get_bmi_label(self) -> str:
+        bmi_value = self.get_bmi_value()
+
+        if (
+            self.underweight_range[0] <= bmi_value
+            and bmi_value < self.underweight_range[1]
+        ):
+            return "underweight"
+        elif self.normal_range[0] <= bmi_value and bmi_value < self.normal_range[1]:
+            return "normal"
+        elif (
+            self.overweight_range[0] <= bmi_value
+            and bmi_value < self.overweight_range[1]
+        ):
+            return "overweight"
+        else:
+            raise ValueError("Invalid BMI value")
--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from fastapi.exceptions import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from .bmi_calculator import BMICalculator
 
 app = FastAPI()
 
@@ -11,4 +14,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.get('/')
+
+# Data structure for response
+class BMIResult(BaseModel):
+    bmi: float
+    label: str
+
+
+@app.get("/", summary="calculate and label bmi")
+def calculate_bmi(weight: float, height: float) -> BMIResult:
+    if weight <= 0:
+        raise HTTPException(400, "Weight should be > 0 kg")
+    if height <= 0:
+        raise HTTPException(400, "Height should be > 0 m")
+
+    bmi_calc = BMICalculator(weight, height)
+
+    return BMIResult(bmi=bmi_calc.get_bmi_value(), label=bmi_calc.get_bmi_label())
